d58a7bea5b147accbd7461e603539287201c60e0
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-04-24 14:32:30 +0100

    added main menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        float enter = Input.GetAxis("Enter");
+        float exit = Input.GetAxis("Exit");
+        Debug.Log(enter);
+        Debug.Log(exit);
+
+        if (enter > 0)
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+
+        if (exit > 0)
+        {
+            Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
