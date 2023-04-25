69aba962d92d5153bf7c796426c5276516bb5340
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-04-25 12:59:07 +0100

    added excavator digging dirt

--- Assets/Scripts/Excavator/DirtScooper.cs
+++ Assets/Scripts/Excavator/DirtScooper.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class DirtScooper : MonoBehaviour
 {
     public bool bucket_wheel_turning;
+    public Vector3 shrink = new Vector3(0.05f, 0.05f, 0.05f);
+    public Vector3 minimum_size = new Vector3(0.1f, 0.1f, 0.1f);
 
     private void OnTriggerStay(Collider other)
     {
         if (bucket_wheel_turning)
         {
+            if (other.gameObject.name == "dirt_ball")
+            {
+                other.gameObject.transform.localScale -= shrink;
 
+                if (other.gameObject.transform.localScale.x < minimum_size.x &&
+                    other.gameObject.transform.localScale.y < minimum_size.y &&
+                    other.gameObject.transform.localScale.z < minimum_size.z)
+                {
+                    Destroy(other.gameObject);
+                }
+            }
         }
     }
 }
