a169b0fd6f8c0137b394f0c693d826767893caf7
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-04-26 17:47:41 +0100

    added dirt counter

--- Assets/Scripts/Excavator/DirtScooper.cs
+++ Assets/Scripts/Excavator/DirtScooper.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DirtScooper : MonoBehaviour
 {
     public bool bucket_wheel_turning;
     public Vector3 shrink = new Vector3(0.03f, 0.03f, 0.03f);
     public Vector3 minimum_size = new Vector3(0.5f, 0.5f, 0.5f);
+    public int dirt_counter = 0;
+    public TMP_Text dirt_counter_text;
 
     private void OnTriggerStay(Collider other)
     {
@@ -15,6 +18,9 @@ public class DirtScooper : MonoBehaviour
             if (other.gameObject.tag == "DirtBall")
             {
                 other.gameObject.transform.localScale -= shrink;
+                dirt_counter++;
+                dirt_counter_text.text = dirt_counter.ToString();
+                Debug.Log(dirt_counter);
 
                 if (other.gameObject.transform.localScale.x < minimum_size.x &&
                     other.gameObject.transform.localScale.y < minimum_size.y &&
