db2fae2729a71a140258f6c704ce5790562fae80
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-04-28 11:03:47 +0100

    added dirtballs rigidbodies when they are squished to a certain size

--- Assets/Scripts/Excavator/DirtSquisher.cs
+++ Assets/Scripts/Excavator/DirtSquisher.cs
@@ -6,12 +6,25 @@ public class DirtSquisher : MonoBehaviour
 {
     public Vector3 squish = new Vector3(0.3f, 0.3f, 0.3f);
     public Vector3 minimum_size = new Vector3(0.5f, 0.5f, 0.5f);
+    public Vector3 size_for_rigidbody = new Vector3(5.0f, 5.0f, 5.0f);
 
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "DirtBall")
         {
             other.gameObject.transform.localScale -= squish;
+
+            if (other.gameObject.transform.localScale.x < size_for_rigidbody.x &&
+                       other.gameObject.transform.localScale.y < size_for_rigidbody.y &&
+                       other.gameObject.transform.localScale.z < size_for_rigidbody.z)
+            {
+                if (other.gameObject.GetComponent<Rigidbody>() == null)
+                {
+                    other.gameObject.AddComponent(typeof(Rigidbody));
+                    other.gameObject.GetComponent<Rigidbody>().drag = 1.0f;
+                    other.gameObject.GetComponent<Rigidbody>().angularDrag = 1.0f;
+                }
+            }
 
             if (other.gameObject.transform.localScale.x < minimum_size.x &&
                 other.gameObject.transform.localScale.y < minimum_size.y &&
