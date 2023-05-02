0436bb8ddf93ddea1a43c48ab657379291f8d4df
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-02 11:16:04 +0100

    added music

---
+++ Assets/Scripts/Music Player/MusicPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public float music_timer = 10.0f;
+    public int track_selection = 1;
+
+    public AudioSource music_1;
+    public AudioSource music_2;
+    public AudioSource music_3;
+
+    void Update()
+    {
+        if (!music_1.isPlaying && !music_2.isPlaying && !music_3.isPlaying)
+        {
+            music_timer -= Time.deltaTime;
+        }
+
+        if (music_timer <= 0.0f)
+        {
+            playMusic();
+        }
+    }
+
+    void playMusic()
+    {
+        if (track_selection == 1)
+        {
+            music_1.Play();
+        }
+        else if (track_selection == 2)
+        {
+            music_2.Play();
+        }
+        else if (track_selection == 3)
+        {
+            music_3.Play();
+        }
+
+        track_selection += 1;
+        
+        if (track_selection > 3)
+        {
+            track_selection = 1;
+        }
+
+        music_timer = 30.0f;
+    }
+}
