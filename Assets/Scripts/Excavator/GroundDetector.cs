8b89b05b978c6d4269ad4f9dcf19a1fdeda1263e
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-03 12:50:37 +0100

    added excavator resetting its rotation if it topples over

---
+++ Assets/Scripts/Excavator/GroundDetector.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetector : MonoBehaviour
+{
+    public bool off_ground_timer_running = false;
+    public float off_ground_timer = 3.0f;
+
+    public GameObject excavator;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            off_ground_timer_running = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            off_ground_timer_running = true;
+        }
+    }
+
+    void Update()
+    {
+        if (off_ground_timer_running)
+        {
+            off_ground_timer -= Time.deltaTime;
+
+            if (off_ground_timer <= 0)
+            {
+                resetExcavator();
+                off_ground_timer = 3.0f;
+                off_ground_timer_running = false;
+            }
+        }
+    }
+
+    void resetExcavator()
+    {
+        Quaternion upright = Quaternion.Euler(0, 0, 0);
+
+        excavator.transform.rotation = upright;
+
+        Debug.Log("reset excavator rotation");
+    }
+}
