97f7d87ac84ec53580ed44412aadbc8426b63fdb
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-07 17:23:42 +0100

    removed more old tutorial content

--- Assets/Scripts/Game Controller/SetUpCameras.cs
+++ Assets/Scripts/Game Controller/SetUpCameras.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class SetUpCameras : MonoBehaviour
 {
-    public Camera FollowCamera;
-    public Camera StaticCamera;
-    public Camera StaticCamera2;
-    public Camera StaticCamera3;
-    public Camera FollowCamera2;
+    public Camera PlayerCamera;
+    public Camera ExcavatorCamera;
     public Camera CabCamera;
     public Camera WheelCamera;
     public Camera HeliCamera;
@@ -18,20 +15,14 @@ public class SetUpCameras : MonoBehaviour
     {
         GameObject PlayerCharacter = GameObject.FindGameObjectWithTag("Player");
 
-        FollowCamera.enabled = true;
-        StaticCamera.enabled = false;
-        StaticCamera2.enabled = false;
-        StaticCamera3.enabled = false;
-        FollowCamera2.enabled = false;
+        PlayerCamera.enabled = true;
+        ExcavatorCamera.enabled = false;
         CabCamera.enabled = false;
         WheelCamera.enabled = false;
         HeliCamera.enabled = false;
 
         PlayerCharacter.GetComponent<AudioListener>().enabled = true;
-        StaticCamera.GetComponent<AudioListener>().enabled = false;
-        StaticCamera2.GetComponent<AudioListener>().enabled = false;
-        StaticCamera3.GetComponent<AudioListener>().enabled = false;
-        FollowCamera2.GetComponent<AudioListener>().enabled = false;
+        ExcavatorCamera.GetComponent<AudioListener>().enabled = false;
         CabCamera.GetComponent<AudioListener>().enabled = false;
         WheelCamera.GetComponent<AudioListener>().enabled = false;
         HeliCamera.GetComponent<AudioListener>().enabled = false;
