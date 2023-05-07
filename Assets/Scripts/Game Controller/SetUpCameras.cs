9abcedc02b25c1bfb83483e3e0b9f706a44336ea
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-07 20:37:23 +0100

    added option to toggle minimap

--- Assets/Scripts/Game Controller/SetUpCameras.cs
+++ Assets/Scripts/Game Controller/SetUpCameras.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class SetUpCameras : MonoBehaviour
 {
+    public bool toggled_minimap = false;
+
     public Camera PlayerCamera;
     public Camera ExcavatorCamera;
     public Camera CabCamera;
     public Camera WheelCamera;
     public Camera HeliCamera;
-    public Camera pip_cam;
+    public Camera minimap_cam;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +23,30 @@ public class SetUpCameras : MonoBehaviour
         CabCamera.enabled = false;
         WheelCamera.enabled = false;
         HeliCamera.enabled = false;
-        pip_cam.enabled = false;
+        minimap_cam.enabled = true;
 
         PlayerCharacter.GetComponent<AudioListener>().enabled = true;
         ExcavatorCamera.GetComponent<AudioListener>().enabled = false;
         CabCamera.GetComponent<AudioListener>().enabled = false;
         WheelCamera.GetComponent<AudioListener>().enabled = false;
         HeliCamera.GetComponent<AudioListener>().enabled = false;
+    }
+
+    void Update()
+    {
+        float map_toggle = Input.GetAxis("MapToggle");
+
+        if (map_toggle > 0 && !toggled_minimap)
+        {
+            minimap_cam.enabled = !minimap_cam.enabled;
+            toggled_minimap = true;
+        }
+        else if (map_toggle <= 0)
+        {
+            if (toggled_minimap)
+            {
+                toggled_minimap = false;
+            }
+        }
     }
 }
