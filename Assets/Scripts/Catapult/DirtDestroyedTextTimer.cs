6451a2911b189faefdbd6ccfb0c171a49b40c75d
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-11 20:50:38 +0100

    added dirt destroyed text

---
+++ Assets/Scripts/Catapult/DirtDestroyedTextTimer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DirtDestroyedTextTimer : MonoBehaviour
+{
+    public bool timer_running = false;
+    public bool fading_out = false;
+    public float timer = 3.0f;
+
+    public Color colour;
+    public Color original_colour;
+
+    private void Start()
+    {
+        colour = GetComponent<TMP_Text>().color;
+        original_colour = colour;
+    }
+
+    private void Update()
+    {
+        if (timer_running)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                fading_out = true;
+                timer_running = false;
+                timer = 3.0f;
+            }
+        }
+
+        if (fading_out)
+        {
+            fadeOut();
+        }
+    }
+
+    private void fadeOut()
+    {
+        colour.a -= Time.deltaTime;
+        GetComponent<TMP_Text>().color = colour;
+
+        if (colour.a <= 0)
+        {
+            GetComponent<TMP_Text>().color = original_colour;
+            colour = original_colour;
+            GetComponent<TMP_Text>().enabled = false;
+            fading_out = false;
+        }
+    }
+}
--- Assets/Scripts/Catapult/Kill.cs
+++ Assets/Scripts/Catapult/Kill.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Kill : MonoBehaviour
 {
+    public bool destroying = false;
+
     public Transform cam_parent;
     public Camera player_cam;
     public Camera projectile_cam;
+    public TMP_Text dirt_destroyed;
 
     void Awake()
     {
         cam_parent = GameObject.Find("Cameras").transform;
         player_cam = GameObject.Find("Player Camera").GetComponent<Camera>() as Camera;
         projectile_cam = GameObject.Find("projectile_cam").GetComponent<Camera>() as Camera;
+        dirt_destroyed = GameObject.Find("dirt_destroyed").GetComponent<TMP_Text>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -20,13 +25,33 @@ public class Kill : MonoBehaviour
         if (collision.gameObject.tag == "DirtBall" || collision.gameObject.tag == "DirtBall2" || collision.gameObject.tag == "DirtBall3")
         {
             Destroy(collision.gameObject);
+
+            if (!dirt_destroyed.enabled)
+            {
+                dirt_destroyed.enabled = true;
+                dirt_destroyed.GetComponent<DirtDestroyedTextTimer>().timer_running = true;
+            }
         }
 
-        projectile_cam.enabled = false;
-        player_cam.enabled = true;
+        if (projectile_cam.enabled)
+        {
+            projectile_cam.enabled = false;
+        }
 
-        projectile_cam.transform.parent = cam_parent;
+        if (!player_cam.enabled)
+        {
+            player_cam.enabled = true;
+        }
 
-        Destroy(gameObject, 3);
+        if (projectile_cam.transform.parent != cam_parent)
+        {
+            projectile_cam.transform.parent = cam_parent;
+        }
+
+        if (!destroying)
+        {
+            Destroy(gameObject, 3);
+            destroying = true;
+        }
     }
 }
