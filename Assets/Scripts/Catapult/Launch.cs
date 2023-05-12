12a614647a8e6e16b93f2d15472333fddb4c1b8e
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-12 20:24:29 +0100

    work on catapult animation

--- Assets/Scripts/Catapult/Launch.cs
+++ Assets/Scripts/Catapult/Launch.cs
@@ -8,9 +8,13 @@ public class Launch : MonoBehaviour
     public GameObject projectile;
     public float projectileForce = 800.0f;
     public float catapult_cooldown = 0;
+    public float reloading_timer = 7.0f;
+    public float stop_reloading_timer = 2.0f;
     GameObject missile;
     public bool missile_is_live = false;
     public bool player_in_range = false;
+    public bool reloading_timer_running;
+    public bool stop_reloading_timer_running;
 
     public Transform projectile_cam_pos;
 
@@ -61,6 +65,7 @@ public class Launch : MonoBehaviour
             launch_audio_source.Play();
 
             anim.SetBool(hash.launchingBool, true);
+            reloading_timer_running = true;
 
             if (catapult_instruction.enabled)
             {
@@ -77,12 +82,38 @@ public class Launch : MonoBehaviour
                 minimap_toggle_instruction.enabled = false;
             }
 
-            catapult_cooldown = 8.0f;
+            catapult_cooldown = 9.0f;
         }
 
         if (catapult_cooldown > 0)
         {
             catapult_cooldown -= Time.deltaTime;
+        }
+
+        if (reloading_timer_running)
+        {
+            reloading_timer -= Time.deltaTime;
+
+            if (reloading_timer <= 0)
+            {
+                anim.SetBool(hash.launchingBool, false);
+                anim.SetBool(hash.reloadingBool, true);
+                reloading_timer_running = false;
+                reloading_timer = 7.0f;
+                stop_reloading_timer_running = true;
+            }
+        }
+
+        if (stop_reloading_timer_running)
+        {
+            stop_reloading_timer -= Time.deltaTime;
+
+            if (stop_reloading_timer <= 0)
+            {
+                anim.SetBool(hash.reloadingBool, false);
+                stop_reloading_timer_running = false;
+                stop_reloading_timer = 2.0f;
+            }
         }
     }
 }
