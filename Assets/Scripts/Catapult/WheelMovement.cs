32ec72f9f7dcadabab36e994f466b75bf4f06f18
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2023-05-13 19:44:02 +0100

    removed commented code

--- Assets/Scripts/Catapult/WheelMovement.cs
+++ Assets/Scripts/Catapult/WheelMovement.cs
@@ -8,10 +8,6 @@ public class WheelMovement : MonoBehaviour
     public float x_movement = 0;
     public float speed = 0;
 
-    //public Vector3 pre_position;
-    //public Vector3 post_position;
-    //public Vector3 local_pre_position;
-    //public Vector3 local_post_position;
     public Vector3 velocity;
     public Vector3 local_velocity;
     public Animator anim;
@@ -19,12 +15,6 @@ public class WheelMovement : MonoBehaviour
 
     public HashIDs hash;
     public EnterTheExcavator enter_the_excavator;
-
-    void Start()
-    {
-        //pre_position = transform.position;
-        //local_pre_position = transform.worldToLocalMatrix.MultiplyVector(pre_position);
-    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -50,33 +40,6 @@ public class WheelMovement : MonoBehaviour
             local_velocity = transform.InverseTransformDirection(velocity);
             speed = -local_velocity.z;
             anim.SetFloat(hash.wheelSpeedFloat, speed);
-
-
-            //Vector3 local_forward = transform.worldToLocalMatrix.MultiplyVector(transform.forward);
-            //Debug.DrawRay(transform.position, transform.forward);
-            //post_position = transform.position;
-            //local_post_position = transform.worldToLocalMatrix.MultiplyVector(post_position);
-
-            //if (local_post_position.z < local_pre_position.z)
-            //{    
-            //    x_movement = local_post_position.z - local_pre_position.z;
-            //    pre_position = post_position;
-            //    local_pre_position = transform.worldToLocalMatrix.MultiplyVector(pre_position);
-            //    //Debug.Log("moved forward");
-            //    //Debug.Log(x_movement);
-            //}
-            //else if (local_post_position.z > local_pre_position.z)
-            //{
-            //    x_movement = local_post_position.z - local_pre_position.z;
-            //    pre_position = post_position;
-            //    local_pre_position = transform.worldToLocalMatrix.MultiplyVector(pre_position);
-            //    //Debug.Log("moved backwards");
-            //    //Debug.Log(x_movement);
-            //}
-            //else
-            //{
-            //    x_movement = 0;
-            //}
         }
     }
 }
